640f001ae33e7a47c3cdb55c48413dee9400e460
Zuyang <duguzuyang@gmail.com>
2015-06-02 13:09:52 +0800

    Update yeelink.ino
    Add more sensors

--- yeelink.ino
+++ yeelink.ino
@@ -6,30 +6,44 @@
 #include <Ethernet.h>
 #include <Wire.h>
 #include <math.h>
+#include <dht.h> 
+#define dht_dpin  A0
+#define measurePin A2 
 
-int BH1750address = 0x23;
+dht DHT;
+
 byte buff[2];
 
 // for yeelink api
-#define APIKEY         "9cdf51696fa9ddfacdf819033a5f2f63" // replace your yeelink api key here
-#define DEVICEID       4 // replace your device ID
-#define SENSORID       7 // replace your sensor ID
+#define APIKEY         "bfe34a96d4a264e0bbfee0e9c7151d3a" // replace your yeelink api key here
+#define DEVICEID       21474 // replace your device ID
+#define THERMID        37851 // replace your sensor ID
+#define HUMIDID        37855 // replace your sensor ID
+
+char lightPin = A1;
+int ledPower = 2;    // 连接数字口2
+char thermID[] = "37851";
+char humidID[] = "37855";
+char lightID[] = "37958";
+char dustID[]  = "38078";
+int i=4;
 
 // assign a MAC address for the ethernet controller.
 byte mac[] = { 0x00, 0x1D, 0x72, 0x82, 0x35, 0x9D};
 // initialize the library instance:
 EthernetClient client;
-//char server[] = "api.yeelink.net";   // name address for yeelink API
-IPAddress server(202,136,60,231);      // numeric IP for api.pachube.com
+char server[] = "api.yeelink.net";   // name address for yeelink API
+//IPAddress server(42,96,164,52);      // numeric IP for api.yeelink.net
 
 unsigned long lastConnectionTime = 0;          // last time you connected to the server, in milliseconds
 boolean lastConnected = false;                 // state of the connection last time through the main loop
-const unsigned long postingInterval = 30*1000; // delay between 2 datapoints, 30s
+const unsigned long postingInterval = 1*1000; // delay between 2 datapoints, 30s
 
 void setup() {
+  pinMode(ledPower,OUTPUT);
   Wire.begin();
   // start serial port:
-  Serial.begin(57600);
+  Serial.begin(115200);
   // start the Ethernet connection with DHCP:
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
@@ -45,6 +59,7 @@ void loop() {
   // if there's incoming data from the net connection.
   // send it out the serial port.  This is for debugging
   // purposes only:
+  DHT.read11(dht_dpin);
   if (client.available()) {
     char c = client.read();
     Serial.print(c);
@@ -62,11 +77,41 @@ void loop() {
   // your last connection, then connect again and send data:
   if(!client.connected() && (millis() - lastConnectionTime > postingInterval)) {
     // read sensor data, replace with your code
-    int sensorReading = readLightSensor();
-    Serial.print("yeelink:");
-    Serial.println(sensorReading);
+    int thermval = getthermval();
+    int humidval = gethumidval();
+    int lightval = getlightval(lightPin);
+    int dustval  = averageDust();
+    Serial.println("yeelink:");
+    Serial.print(thermval);
+    Serial.println(" C");
+    Serial.print(humidval);
+    Serial.println(" %");
+    Serial.println(lightval);
+    Serial.print(dustval);
+    Serial.println(" ug/m3");
     //send data to server
-    sendData(sensorReading);
+    if(humidval <= 100 && humidval > 0 && dustval > 0){
+      if(i % 4 == 0){
+        sendData(thermval,thermID);
+        Serial.println("Sending therm data");
+        i++;
+      }
+      else if(i % 4 == 1){
+        sendData(lightval,lightID);
+        Serial.println("Sending light data");
+        i++;
+      }
+      else if(i % 4 == 2){
+        sendData(dustval,dustID);
+        Serial.println("Sending dust data");
+        i++;
+      }
+      else{
+        sendData(humidval,humidID);
+        Serial.println("Sending humid data");
+        i -= 3;
+      }
+    }
   }
   // store the state of the connection for next time through
   // the loop:
@@ -74,7 +119,7 @@ void loop() {
 }
 
 // this method makes a HTTP connection to the server:
-void sendData(int thisData) {
+void sendData(int Data,char sensorID[]) {
   // if there's a successful connection:
   if (client.connect(server, 80)) {
     Serial.println("connecting...");
@@ -82,7 +127,7 @@ void sendData(int thisData) {
     client.print("POST /v1.0/device/");
     client.print(DEVICEID);
     client.print("/sensor/");
-    client.print(SENSORID);
+    client.print(sensorID);
     client.print("/datapoints");
     client.println(" HTTP/1.1");
     client.println("Host: api.yeelink.net");
@@ -95,7 +140,7 @@ void sendData(int thisData) {
 
     // calculate the length of the sensor reading in bytes:
     // 8 bytes for {"value":} + number of digits of the data:
-    int thisLength = 10 + getLength(thisData);
+    int thisLength = 10 + getLength(Data);
     client.println(thisLength);
     
     client.println("Content-Type: application/x-www-form-urlencoded");
@@ -104,7 +149,52 @@ void sendData(int thisData) {
 
     // here's the actual content of the PUT request:
     client.print("{\"value\":");
-    client.print(thisData);
+    client.print(Data);
+    client.println("}");
+  } 
+  else {
+    // if you couldn't make a connection:
+    Serial.println("connection failed");
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+  }
+   // note the time that the connection was made or attempted:
+  lastConnectionTime = millis();
+}
+
+// this method makes a HTTP connection to the server:
+void sendhumidData(int humidData) {
+  // if there's a successful connection:
+  if (client.connect(server, 80)) {
+    Serial.println("connecting...");
+    // send the HTTP PUT request:
+    client.print("POST /v1.0/device/");
+    client.print(DEVICEID);
+    client.print("/sensor/");
+    client.print(37855);
+    client.print("/datapoints");
+    client.println(" HTTP/1.1");
+    client.println("Host: api.yeelink.net");
+    client.print("Accept: *");
+    client.print("/");
+    client.println("*");
+    client.print("U-ApiKey: ");
+    client.println(APIKEY);
+    client.print("Content-Length: ");
+
+    // calculate the length of the sensor reading in bytes:
+    // 8 bytes for {"value":} + number of digits of the data:
+    int thisLength = 10 + getLength(humidData);
+    client.println(thisLength);
+    
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.println("Connection: close");
+    client.println();
+
+    // here's the actual content of the PUT request:
+    client.print("{\"value\":");
+    client.print(humidData);
     client.println("}");
   } 
   else {
@@ -138,44 +228,66 @@ int getLength(int someValue) {
   return digits;
 }
 
-///////////////////////////////////////////////////////////////////////////
-// get data from light sensor
-// you can replace this code for your sensor 
-int readLightSensor()
-{
-  uint16_t val=0;
-  BH1750_Init(BH1750address);
-  delay(200);
-  if(2==BH1750_Read(BH1750address))
-  {
-    val=((buff[0]<<8)|buff[1])/1.2;
-  }
-
-  Serial.print("Sensor value is: ");
-  Serial.println((int)val);
-  
+int getlightval(char pin) {
+  //take average of 3 readings as the final value
+  int val1 = 0, val2 = 0, val3 = 0, val = 0;
+  val1 = analogRead(pin);
+  val2 = analogRead(pin);
+  val3 = analogRead(pin);
+  val = (val1 + val2 + val3) / 3;
   return val;
 }
 
-int BH1750_Read(int address) //
-{
-  int i=0;
-  Wire.beginTransmission(address);
-  Wire.requestFrom(address, 2);
-  while(Wire.available()) //
-  {
-    buff[i] = Wire.read();  // receive one byte
-    i++;
-  }
-  Wire.endTransmission();  
-  return i;
+int getthermval(){
+  int val1 = 0, val2 = 0, val3 = 0, val = 0;
+  val1 = DHT.temperature;
+  val2 = DHT.temperature;
+  val3 = DHT.temperature;
+  val = (val1 + val2 + val3) / 3;
+  return val;
 }
 
-void BH1750_Init(int address) 
-{
-  Wire.beginTransmission(address);
-  Wire.write(0x10);//1lx reolution 120ms
-  Wire.endTransmission();
+int gethumidval(){
+  int humidval = DHT.humidity;
+  int val1 = 0, val2 = 0, val3 = 0, val = 0;
+  val1 = DHT.humidity;
+  val2 = DHT.humidity;
+  val3 = DHT.humidity;
+  val = (val1 + val2 + val3) / 3;
+  return val;
 }
- 
-
+  
+float getdustval(){
+  int samplingTime = 280;
+  int deltaTime = 40;
+  int sleepTime = 9680;
+  
+  float voMeasured = 0;
+  float calcVoltage = 0;
+  float dustDensity = 0;
+  
+  digitalWrite(ledPower,LOW);       //开启内部LED
+  delayMicroseconds(samplingTime);  // 开启LED后的280us的等待时间
+  
+  voMeasured = analogRead(measurePin);   // 读取模拟值
+  
+  delayMicroseconds(deltaTime);        //  40us等待时间
+  digitalWrite(ledPower,HIGH);         // 关闭LED
+  delayMicroseconds(sleepTime);
+  
+  // 0 - 5V mapped to 0 - 1023 integer values
+  // recover voltage
+  calcVoltage = voMeasured * (5.0 / 1024.0);   //将模拟值转换为电压值
+  // linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
+  // Chris Nafis (c) 2012
+  dustDensity = 0.17 * calcVoltage - 0.1;
+  return dustDensity;
+}
+  
+float averageDust(){
+  float val1 = getdustval();
+  float val2 = getdustval();
+  float val3 = getdustval();
+  int val = 1000 * (val1 + val2 + val3)/3;   //  ug/m3
+  return val;
+}
